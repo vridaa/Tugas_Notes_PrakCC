348cfc222993a4954202f19435d3d2694e25efe7
vrida pusparani <vridapusparani@gmail.com>
2025-03-13 03:22:37 +0700

    Migrasi & pembaruan frontend

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("notes_db", "root", "", {
-    host: "localhost",
+const db = new Sequelize("notes-rani-db", "root", "rani123", {
+    host: "34.121.186.109",
     dialect: "mysql"
 })
 
